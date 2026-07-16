e79de21506f521d6964426fa49b8e5a44aa746f5
tsiunel1983 <tsiunel1983@gmail.com>
2026-07-16 20:22:56 +0300

    практика работы с grpcio

---
+++ grpcio_open_debit_card_account.py
@@ -0,0 +1,29 @@
+import grpc
+
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserRequest, CreateUserResponse
+from contracts.services.gateway.users.users_gateway_service_pb2_grpc import UsersGatewayServiceStub
+from contracts.services.gateway.accounts.accounts_gateway_service_pb2_grpc import AccountsGatewayServiceStub
+from contracts.services.gateway.accounts.rpc_open_debit_card_account_pb2 import OpenDebitCardAccountRequest, OpenDebitCardAccountResponse
+from tools.fakers import fake
+
+channel = grpc.insecure_channel('localhost:9003')
+
+users_gateway_service = UsersGatewayServiceStub(channel)
+account_gateway_service = AccountsGatewayServiceStub(channel)
+
+create_user_request = CreateUserRequest(
+    email=fake.email(),
+    first_name=fake.first_name(),
+    last_name=fake.last_name(),
+    middle_name=fake.first_name(),
+    phone_number=fake.phone_number()
+)
+create_user_response: CreateUserResponse = users_gateway_service.CreateUser(create_user_request)
+print("create_user_response: ", create_user_response)
+
+open_debit_card_account_request = OpenDebitCardAccountRequest(
+    user_id=create_user_response.user.id
+)
+open_debit_card_account_response:OpenDebitCardAccountResponse = account_gateway_service.OpenDebitCardAccount(open_debit_card_account_request)
+print("open_debit_card_account_request: ", open_debit_card_account_response)
+
