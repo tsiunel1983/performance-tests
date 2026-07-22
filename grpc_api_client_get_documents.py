828b33655ee4f4f6cd707334ba47c6e0aabf86ad
tsiunel1983 <tsiunel1983@gmail.com>
2026-07-22 15:52:26 +0300

    Практикуемся в использовании gRPC API клиентов

---
+++ grpc_api_client_get_documents.py
@@ -0,0 +1,40 @@
+from grpc_api_client_issue_physical_card import accounts_gateway_client
+from clients.grpc.gateway.cards.client import build_cards_gateway_grpc_client
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
+from clients.grpc.gateway.documents.client import build_documents_gateway_grpc_client
+
+users_gateway_client = build_users_gateway_grpc_client()
+cards_gateway_client = build_cards_gateway_grpc_client()
+contract_documents_gateway_client = build_documents_gateway_grpc_client()
+tariff_documents_gateway_client = build_documents_gateway_grpc_client()
+
+create_user_response = users_gateway_client.create_user()
+"""
+Используем метод create_user
+Создаем пользователя
+"""
+print("Create user response: ", create_user_response)
+
+open_credit_card_account_response = accounts_gateway_client.open_credit_card_account(
+    user_id=create_user_response.user.id)
+"""
+Используем метод open_credit_card_account
+Открываем кредитный счет
+"""
+print("Open credit card account response: ", open_credit_card_account_response)
+
+get_tariff_document_response = tariff_documents_gateway_client.get_tariff_document(
+    account_id=open_credit_card_account_response.account.id)
+"""
+Используем метод get_contract_document
+Получаем документ о тарифе кредитного счета.
+"""
+print("Get tariff document response: ", get_tariff_document_response)
+
+get_contract_document_response = contract_documents_gateway_client.get_contract_document(
+    account_id=open_credit_card_account_response.account.id)
+"""
+Используем метод get_contract_document
+Получаем контракт об открытии кредитного счета.
+"""
+print("Get contract document response: ", get_contract_document_response)
--- grpc_api_client_issue_physical_card.py
+++ grpc_api_client_issue_physical_card.py
@@ -1,32 +1,27 @@
-from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
-from clients.grpc.gateway.cards.client import build_cards_gateway_grpc_client
+# Импортируем фабричные функции для создания API-клиентов
 from clients.grpc.gateway.accounts.client import build_accounts_gateway_grpc_client
+from clients.grpc.gateway.cards.client import build_cards_gateway_grpc_client
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
 
+# Создаём API-клиенты для работы с сервисами Users, Accounts и Cards
 users_gateway_client = build_users_gateway_grpc_client()
 cards_gateway_client = build_cards_gateway_grpc_client()
 accounts_gateway_client = build_accounts_gateway_grpc_client()
 
-"""
-Используем метод create_user
-Создаем пользователя
-"""
+# Шаг 1. Создаём пользователя
 create_user_response = users_gateway_client.create_user()
-print("Create user response: ",create_user_response)
+print('Create user response:', create_user_response)
 
-"""
-Используем метод open_debit_card_account
-Открываем дебитовый счет
-"""
+# Шаг 2. Открываем дебетовый счёт для только что созданного пользователя
 open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(
-    user_id=create_user_response.user.id,)
-print("Open debit card account response: ",open_debit_card_account_response)
+    user_id=create_user_response.user.id
+)
+print('Open debit card account response:', open_debit_card_account_response)
 
-"""
-Используем метод issue_physical_card
-Выпускаем физическую карту
-"""
+# Шаг 3. Выпускаем физическую карту для указанного счёта и пользователя
 issue_physical_card_response = cards_gateway_client.issue_physical_card(
     user_id=create_user_response.user.id,
-    account_id=open_debit_card_account_response.account.id,
+    account_id=open_debit_card_account_response.account.id
 )
-print("Issue physical card response: ",issue_physical_card_response)
+print('Issue physical card response:', issue_physical_card_response)
+
